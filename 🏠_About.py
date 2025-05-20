9c644235d1fea599faeb19d2fded562270e12ca7
apliabelina <apliabelina@gmail.com>
2025-05-20 09:26:17 +0700

    Update 🏠_About.py

--- 🏠_About.py
+++ 🏠_About.py
@@ -5,7 +5,6 @@ st.set_page_config(page_title="Dashboard", layout="wide")
 
 st.title("Dashboard Analisis Konversi Pinjaman")
 
-st.write("Streamlit version:", st.__version__)
 
 st.markdown("""
 Selamat datang di **Dashboard Analisis Konversi Pinjaman**.  
